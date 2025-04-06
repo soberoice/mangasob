4485159ef98b664e5f2c99ae2c3c2140bb3e66c3
royce <royakhabhoa@gmail.com>
2025-04-06 16:26:55 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   const getManga = async () => {
     try {
-      const baseUrl = "https://proxy.hoppscotch.io/https://api.mangadex.org";
+      const baseUrl =
+        "https://cors-anywhere-vercel-nu-one.vercel.app/api/proxy?https://api.mangadex.org";
       const resp = await axios.get(`${baseUrl}/manga`, {
         params: { title },
       });
