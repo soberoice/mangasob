af993d7d4076cb77292c9bb859c962d7c5437799
royce <royakhabhoa@gmail.com>
2025-04-06 15:34:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+  const title = "Kanojyo to Himitsu to Koimoyou";
+
+  const getManga = async () => {
+    try {
+      const baseUrl = "https://api.mangadex.org";
+      const resp = await axios.get(`${baseUrl}/manga`, {
+        params: { title },
+      });
+
+      setData(resp.data.data);
+      console.log(resp.data.data);
+    } catch (error) {
+      console.error("Error fetching manga:", error);
+    }
+  };
+
+  useEffect(() => {
+    getManga();
+  }, []);
+
+  return (
+    <>
+      {data.map((item) => (
+        <p className="read-the-docs" key={item.id}>
+          {item?.attributes?.title?.en ?? "No English Title"}
+        </p>
+      ))}
+    </>
+  );
+}
+
+export default App;
